0b5d3d053e426036bf9fd99b50f715201d24c0ca
Jesica Schnaiderman <jesica@carolia.local>
2021-01-26 00:33:46 -0300

    segunda entrega navbar

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NavbarComponent from './components/Navbar/index';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <>
+      <NavbarComponent/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,30 @@
+
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import './index.css';
+const NavbarComponent = () =>{
+    return (
+    <>
+        <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+        <Navbar.Brand href="#home">Gyrokinesis</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+        <Nav.Link href="#home">Inicio</Nav.Link>
+            <NavDropdown title="Productos..." id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#categoria1">categoria1</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#categoria2">categoria2</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#categoria3">categoria3</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#contactos">Contactos</Nav.Link>
+        </Nav>
+    </Navbar.Collapse>
+    </Navbar>
+    </>
+);
+}
+
+export default NavbarComponent;
